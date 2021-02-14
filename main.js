8bbc2314b3dd52ebd557290431a179987e3e3ead
djshujja <djshujja@gmail.com>
2021-02-14 23:27:27 +0500

    floating buttons

--- main.js
+++ main.js
@@ -1,4 +1,36 @@
 window.onload = () => {
+  //Get the button:
+  topButton = document.getElementById("top-button");
+  whatsappButton = document.getElementById("whatsapp-button");
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  topButton.addEventListener("click", () => {
+    topFunction();
+  });
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      topButton.style.display = "block";
+      whatsappButton.style.display = "block";
+    } else {
+      topButton.style.display = "none";
+      whatsappButton.style.display = "none";
+    }
+  }
+
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+
   // PinchToZoom
   document.addEventListener(
     "touchmove",
